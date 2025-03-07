ef16b50d6a046f5c27c436243771ea33806a6629
hirusha-adi <hirushaadi@gmail.com>
2025-03-07 23:24:01 +0530

    move images

--- src/assets/photos.js
+++ src/assets/photos.js
@@ -74,8 +74,117 @@ export const PhotosJson = {
       "tags": ["Temples", "Buddhist", "Culture"],
       "locations": ["Anuradhapura", "Dhamma Gaveshi Temple"],
       "country": "Sri Lanka"
-    }
+    },
 
-    // 
+    // [Sri Lanka] Anuradhapura - Isurumuniya Temple
+    {
+      "name": "",
+      "filename": "378.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "379.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "380.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "381.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "392.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "393.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "394.jpg",
+      "tags": ["Animals", "Monkeys"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "395.jpg",
+      "tags": ["Animals", "Monkeys"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "396.jpg",
+      "tags": ["Animals", "Monkeys"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "397.jpg",
+      "tags": ["Animals", "Monkeys"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "398.jpg",
+      "tags": ["Animals", "Monkeys"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "399.jpg",
+      "tags": ["Ponds"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "400.jpg",
+      "tags": ["Ponds"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "401.jpg",
+      "tags": ["Scenery"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "402.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Isurumuniya Temple"],
+      "country": "Sri Lanka"
+    },
+
+    // [Sri Lanka] Anuradhapura - Jethawanaramaya
+    
+
   ]
 }
