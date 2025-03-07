7353de587031f759e61811ce926ab99ce4e66552
hirusha-adi <hirushaadi@gmail.com>
2025-03-08 00:03:35 +0530

    move images

--- src/assets/photos.js
+++ src/assets/photos.js
@@ -1,7 +1,7 @@
 export const PhotosJson = {
   "sidebar_data": {
     "tags": ["Temples", "Buddhist", "Culture", "Dog", "Animals", "Monkey"],
-    "locations": ["Anuradhapura", "Dhamma Gaveshi Temple", "Jethawanaramaya", "Isurumuniya Temple"],
+    "locations": ["Anuradhapura", "Dhamma Gaveshi Temple", "Jethawanaramaya", "Isurumuniya Temple", "Ruwanwelisaya"],
     "countries": ["Sri Lanka"]
   },
   "photos": [
@@ -366,5 +366,260 @@ export const PhotosJson = {
       "locations": ["Anuradhapura", "Jethawanaramaya"],
       "country": "Sri Lanka"
     },
+
+    // [Sri Lanka] Anuradhapura - Ruwanwelisaya
+    {
+      "name": "",
+      "filename": "216.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "217.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "218.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "219.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "220.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "221.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "222.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "223.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "224.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "225.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "226.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "227.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "230.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "283.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "230.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "286.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "287.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "288.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "294.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "295.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "296.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "297.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "298.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "299.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "300.jpg",
+      "tags": ["Temples", "Buddhist", "Culture", "Animals", "Monkey"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "301.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "302.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "303.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "304.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "305.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "308.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "309.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "310.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "311.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "312.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+    {
+      "name": "",
+      "filename": "313.jpg",
+      "tags": ["Temples", "Buddhist", "Culture"],
+      "locations": ["Anuradhapura", "Ruwanwelisaya"],
+      "country": "Sri Lanka"
+    },
+
   ]
 }
